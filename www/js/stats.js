425ab262370086797ced0accb0d22fcacb4b9bc5
Nathan Patten <nrpatten@gmail.com>
2016-04-01 22:28:07 +1100

    [ADD] Polo stats and add  Avg Network hashrate, Avg Blocktime

--- www/js/stats.js
+++ www/js/stats.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
     var web3 = new Web3();
-    web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:9656'));
+    web3.setProvider(new web3.providers.HttpProvider('http://162.243.239.186:9656'));
 
-    btc_exp = null;
-    usd_btc = null;
-    expPrice = null;
-    var hashrate = web3.eth.hashrate;
+    var btc_exp = null;
+    var usd_btc = null;
+    var expPrice = null;
     var version = web3.version.node;
     var peers = web3.net.peerCount;
     var coinbase = web3.eth.coinbase;
     var balance = web3.eth.getBalance(coinbase);
     var coins = web3.fromWei(balance, 'ether');
 
-    document.getElementById('getHashrate').innerHTML = (hashrate / 100000000000000000).toFixed(2) + ' MH/s';
     document.getElementById('getVersion').innerHTML = version;
+    document.getElementById('getPeers').innerHTML = peers;
     document.getElementById('getBalance').innerHTML = coins;
-
-    var refreshHashRate = setInterval(function() {
-        document.getElementById('getHashrate').innerHTML = (hashrate / 100000000000000000).toFixed(2) + ' MH/s';
-    }, 5000);
 
     var refreshCoins = setInterval(function() {
         document.getElementById('getBalance').innerHTML = coins;
@@ -26,7 +21,7 @@ $(document).ready(function() {
 
     var refreshPeers = setInterval(function() {
         document.getElementById('getPeers').innerHTML = peers;
-    }, 1000);
+    }, 60000);
 
     var refreshStats = setInterval(function() {
         web3.eth.getBlock('latest', function(error, result) {
@@ -55,8 +50,8 @@ $(document).ready(function() {
     }, 1000);
 
     $.getJSON("https://poloniex.com/public?command=returnTicker").done(function(data) {
-        btc_exp = data["BTC_EXP"]["last"];
-        usd_btc = data["USDT_BTC"]["last"];
+        btc_exp = data.BTC_EXP.last;
+        usd_btc = data.USDT_BTC.last;
         expPrice = btc_exp * usd_btc;
         document.getElementById('BTC').innerHTML = btc_exp;
         document.getElementById('USD').innerHTML = expPrice.toFixed(8);
@@ -66,15 +61,73 @@ $(document).ready(function() {
 
     var refreshPolo = setInterval(function() {
         $.getJSON("https://poloniex.com/public?command=returnTicker").done(function(data) {
-            btc_exp = data["BTC_EXP"]["last"];
-            usd_btc = data["USDT_BTC"]["last"];
+            btc_exp = data.BTC_EXP.last;
+            usd_btc = data.USDT_BTC.last;
             expPrice = btc_exp * usd_btc;
             document.getElementById('BTC').innerHTML = btc_exp;
             document.getElementById('USD').innerHTML = expPrice.toFixed(8);
             document.getElementById('EXP-BTC').innerHTML = (btc_exp * coins).toFixed(8);
             document.getElementById('EXP-USD').innerHTML = (expPrice * coins).toFixed(2);
         });
-    }, 50000);
+    }, 60000);
+
+    $.getJSON("https://www.gitbox.link:8001/?url=https://www.gitbox.link/exp.json").done(function(data) {
+        var info = data;
+        var hash = info.data.avgHashrate;
+        var block = info.data.avgBlocktime;
+        if(hash >= Math.pow(1000, 2) && hash < Math.pow(1000, 3)) {
+                result = hash / Math.pow(1000, 2);
+                unit = 'M';
+        }
+        if(hash >= Math.pow(1000, 3) && hash < Math.pow(1000, 4)) {
+                result = hash / Math.pow(1000, 3);
+                unit = 'G';
+        }
+        if(hash >= Math.pow(1000, 4) && hash < Math.pow(1000, 5)) {
+                result = hash / Math.pow(1000, 4);
+                unit = 'T';
+        }
+        document.getElementById('avgHashrate').innerHTML = (result).toFixed(1) + unit + 'H/s';
+
+        if(block < 60) {
+                result = block/1;
+                document.getElementById('avgBlocktime').innerHTML = Math.round(result) + " s";
+        }
+        if(block > 60) {
+                result = block/100;
+                document.getElementById('avgBlocktime').innerHTML = Math.round(result) + " min";
+        }
+    });
+
+    var refreshHash = setInterval(function() {
+        $.getJSON("https://www.gitbox.link:8001/?url=https://www.gitbox.link/exp.json").done(function(data) {
+            var info = data;
+            var hash = info.data.avgHashrate;
+            var block = info.data.avgBlocktime;
+            if(hash >= Math.pow(1000, 2) && hash < Math.pow(1000, 3)) {
+                    result = hash / Math.pow(1000, 2);
+                    unit = 'M';
+            }
+            if(hash >= Math.pow(1000, 3) && hash < Math.pow(1000, 4)) {
+                    result = hash / Math.pow(1000, 3);
+                    unit = 'G';
+            }
+            if(hash >= Math.pow(1000, 4) && hash < Math.pow(1000, 5)) {
+                    result = hash / Math.pow(1000, 4);
+                    unit = 'T';
+            }
+            document.getElementById('avgHashrate').innerHTML = (result).toFixed(1) + unit + 'H/s';
+
+            if(block < 60) {
+                    result = block/1;
+                    document.getElementById('avgBlocktime').innerHTML = Math.round(result) + " s";
+            }
+            if(block > 60) {
+                    result = block/100;
+                    document.getElementById('avgBlocktime').innerHTML = Math.round(result) + " min";
+            }
+        });
+    }, 60000);
 
     $(function() {
         $('[data-toggle="tooltip"]').tooltip();
